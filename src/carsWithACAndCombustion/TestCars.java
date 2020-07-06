60d1f817360035aedf8a90f684ad0d2e71ae2fa8
ulempicka <urszula.lempicka@gmail.com>
2020-07-06 15:06:55 +0200

    Commit 11 - updated Cars - created separate method for repeated code in TestCars class

--- src/carsWithACAndCombustion/TestCars.java
+++ src/carsWithACAndCombustion/TestCars.java
@@ -5,22 +5,23 @@ public class TestCars {
         Vehicle[] vehicles = new Vehicle[2];
         vehicles[0] = new Car("Toyota Yaris", 45, 4.5, false);
         vehicles[1] = new Truck("Star 244", 150, 26, false, 1000);
-        double range;
 
         for (Vehicle vehicle : vehicles) {
-            vehicle.showInfo();
-            range = vehicle.calculateRange();
-            System.out.println("Zasięg " + range);
+            displayVehicle(vehicle);
         }
 
         vehicles[0].setAirCond(true);
         vehicles[1].setAirCond(true);
 
         for (Vehicle vehicle : vehicles) {
-            vehicle.showInfo();
-            range = vehicle.calculateRange();
-            System.out.println("Zasięg " + range);
+            displayVehicle(vehicle);
         }
+    }
+
+     private static void displayVehicle(Vehicle vehicle){
+        vehicle.showInfo();
+        double range = vehicle.calculateRange();
+        System.out.println("Zasięg " + range);
     }
 }
 
